6bc8f89a72c71d075138ed104d5d3b95ff6fed46
Player <titan0987tur@gmail.com>
2026-04-03 04:27:39 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,6 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.svoychat.app',
   appName: 'Свой чат',
+  server: {
+    url: 'https://your-chat990.netlify.app',
+    cleartext: true
+  },
   webDir: 'www'
 };
 
