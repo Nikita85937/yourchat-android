cb1971e1783f8932eab37aa8769aaa09581864d2
Player <titan0987tur@gmail.com>
2026-04-11 18:28:17 +0300

    Correction.
    Removed server configuration from Capacitor config.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,6 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.svoychat.app',
   appName: 'Свой чат',
-  server: {
-    url: 'https://your-chat990.netlify.app',
-    cleartext: true
-  },
   webDir: 'www'
 };
 
