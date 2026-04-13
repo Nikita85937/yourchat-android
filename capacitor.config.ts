70d5246ea1180d5af123b356ee8227f852b5a94c
Player <titan0987tur@gmail.com>
2026-04-13 12:13:49 +0300

    New001

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,9 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.svoychat.app',
   appName: 'Свой чат',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    hostname: 'chats-f2c9f.firebaseapp.com',
+    androidScheme: 'https'
+  }
 };
-
-export default config;
